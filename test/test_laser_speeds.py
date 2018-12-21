cf3bdf23e63fbd1e6956370644c7ec1e9a914384
tatarize <tatarize@gmail.com>
2018-12-21 01:40:59 -0800

    Add files via upload

--- test/test_laser_speeds.py
+++ test/test_laser_speeds.py
@@ -4,9 +4,15 @@ import unittest
 
 from k40.k40nano import LaserM2
 
+from k40.k40nano import LaserM1
+
 
 class TestLaserSpeeds(unittest.TestCase):
 
     def test_generate_speed_M2(self):
         b = LaserM2()
         self.assertEqual(b.make_speed(.5), "CV0551401001108644C")
+
+    def test_generate_speed_M1(self):
+        b = LaserM1()
+        self.assertEqual(b.make_speed(.5), "CV167750462391000000000")
